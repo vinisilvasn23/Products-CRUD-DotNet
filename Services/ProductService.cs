09928510cc2e088f4b191c3d00ec2a91d9b9aa01
Vinicius <vini456nascimento@gmail.com>
2024-04-16 15:56:55 -0300

    feat: create product service

---
+++ Services/ProductService.cs
@@ -0,0 +1,54 @@
+using ApiCrud.Data;
+using ApiCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrud.Services
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProductAsync(Guid id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<Product> AddProductAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(Guid id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public bool ProductExists(Guid id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
