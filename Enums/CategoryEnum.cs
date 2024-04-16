a4b1024cf0471ec16044dc99e837ae2960a5f09b
Vinicius <vini456nascimento@gmail.com>
2024-04-16 15:56:33 -0300

    feat: created enum categories

---
+++ Enums/CategoryEnum.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiCrud.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum CategoryEnum
+    {
+        Eletrônicos,
+        Roupas,
+        Alimentos,
+        Livros,
+        Brinquedos,
+        Outros
+    }
+}
